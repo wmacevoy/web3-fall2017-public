318e6ac56922a036905a54ae93fa023a74c51738
Warren MacEvoy <wmacevoy@gmail.com>
2017-09-21 13:40:25 -0600

    open mysql connection using pdo

--- src/config.php
+++ src/config.php
@@ -3,6 +3,9 @@ require_once 'psr4.php';
 
 $user = new model\User();
 
+$db = new PDO('mysql:host=mysql;dbname=starting;charset=utf8mb4', 
+              'user', 'phnz.jcnf.xmmd.nafp.xwna.rwfa.hfzn.cjpn');
+
 session_start();
 
 
